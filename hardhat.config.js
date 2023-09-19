d7ee5881d24be3cc979f8c59322ad39f4cbb92fa
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-19 17:00:26 +0200

    fix repo

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const dotenv = require('dotenv')
-
-
-dotenv.config();
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-  networks: {
-    goerli:{
-      url: process.env.REACT_APP_REAC_APP_ALCHEMY_URL,
-      accounts: [process.env.REAC_APP_PRIVATE_KEY]
-    },
-  },
-  etherscan:{
-    apiKey: process.env.REAC_APP_ALCHEMY_API_KEY,
-  }
+  solidity: "0.8.19",
 };
